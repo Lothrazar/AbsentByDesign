04ed774f9db8062764faf4e13f1469ad6f3ce743
Sam <lothrazar@hotmail.com>
2018-10-16 20:46:00 -0700

    initial forge 1.12.2 setup

---
+++ src/main/java/com/lothrazar/absentbydesign/ModAbsentBD.java
@@ -0,0 +1,29 @@
+package com.lothrazar.absentbydesign;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = ModAbsentBD.MODID )
+public class ModAbsentBD
+{
+    public static final String MODID = "absentbydesign"; 
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
