70773cc3bb3980dde4eba5c13bb4a19a0e8ca311
sam <samson.bassett@gmail.com>
2018-10-20 21:24:52 -0700

    Bash seed add recipes

--- assetCopyORES.bat
+++ assetCopyORES.bat
@@ -4,8 +4,11 @@
 set dest=%1
 set seed=stairs_granite
 
-cp src/main/resources/assets/absentbydesign/blockstates/stairs_granite.json src/main/resources/assets/absentbydesign/blockstates/stairs_%dest%.json
+cp src/main/resources/assets/absentbydesign/blockstates/%seed%.json src/main/resources/assets/absentbydesign/blockstates/stairs_%dest%.json
 
-cp src/main/resources/assets/absentbydesign/models/block/stairs_granite.json src/main/resources/assets/absentbydesign/models/block/stairs_%dest%.json
-cp src/main/resources/assets/absentbydesign/models/item/stairs_granite.json src/main/resources/assets/absentbydesign/models/item/stairs_%dest%.json
+cp src/main/resources/assets/absentbydesign/models/block/%seed%.json src/main/resources/assets/absentbydesign/models/block/stairs_%dest%.json
+
+cp src/main/resources/assets/absentbydesign/models/item/%seed%.json src/main/resources/assets/absentbydesign/models/item/stairs_%dest%.json
+
+cp src/main/resources/assets/absentbydesign/models/recipes/%seed%.json src/main/resources/assets/absentbydesign/models/recipes/stairs_%dest%.json
                               
