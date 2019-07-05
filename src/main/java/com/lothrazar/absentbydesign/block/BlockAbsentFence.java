5ae4165e4c38de48c0536f71ff89d2e86ae403a2
sam <samson.bassett@gmail.com>
2019-07-04 23:29:22 -0700

    1.14 partial

--- src/main/java/com/lothrazar/absentbydesign/block/BlockAbsentFence.java
+++ src/main/java/com/lothrazar/absentbydesign/block/BlockAbsentFence.java
@@ -3,19 +3,25 @@ package com.lothrazar.absentbydesign.block;
 import com.lothrazar.absentbydesign.IHasRecipe;
 import com.lothrazar.absentbydesign.registry.RecipeRegistry;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockFence;
-import net.minecraft.init.Items;
+import net.minecraft.block.FenceBlock;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.item.crafting.IRecipe;
 
-public class BlockAbsentFence extends BlockFence implements IHasRecipe {
+public class BlockAbsentFence extends FenceBlock implements IHasRecipe {
 
   private ItemStack ingredient;
 
   public BlockAbsentFence(Block main, ItemStack i) {
-    super(main.getMaterial(main.getDefaultState()), main.getMaterial(main.getDefaultState()).getMaterialMapColor());
-    this.setSoundType(main.getSoundType());
-    this.setHardness(main.getBlockHardness(main.getDefaultState(), null, null));
+    super(Block.Properties.create(Material.ROCK)
+    .sound(main.getSoundType(null))
+    .hardnessAndResistance(main.getBlockHardness(main.getDefaultState(), null, null)));
+
+//    super(main.getMaterial(main.getDefaultState()), main.getMaterial(main.getDefaultState()).getMaterialMapColor());
+//    this.setSoundType(main.getSoundType());
+//    this.setHardness(main.getBlockHardness(main.getDefaultState(), null, null));
     ingredient = i;
   }
 
---
+++ src/main/java/com/lothrazar/absentbydesign/setup/IProxy.java
@@ -0,0 +1,7 @@
+package com.lothrazar.absentbydesign.setup;
+
+import net.minecraft.world.World;
+
+public interface IProxy {
+	World getClientWorld();
+}
