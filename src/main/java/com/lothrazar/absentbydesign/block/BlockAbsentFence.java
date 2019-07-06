340406424550f523b25dc638957c0689186b1fe9
sam <samson.bassett@gmail.com>
2019-07-05 20:40:31 -0700

    fence connecting works. material decides if its a netherbrick or wooden type

--- src/main/java/com/lothrazar/absentbydesign/block/BlockAbsentFence.java
+++ src/main/java/com/lothrazar/absentbydesign/block/BlockAbsentFence.java
@@ -2,31 +2,31 @@ package com.lothrazar.absentbydesign.block;
 
 import com.lothrazar.absentbydesign.IHasRecipe;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.FenceBlock;
+import net.minecraft.block.FenceGateBlock;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.Direction;
 
 public class BlockAbsentFence extends FenceBlock  {
 // implements IHasRecipe
   private ItemStack ingredient;
 private String rawName;
   public BlockAbsentFence(Block main, ItemStack i, String reg) {
-    super(Block.Properties.create(Material.ROCK)
-    .sound(main.getSoundType(null))
-    .hardnessAndResistance(main.getBlockHardness(main.getDefaultState(), null, null)));
-
+    super(Block.Properties.create(Material.ROCK, MaterialColor.QUARTZ)
+        .sound(main.getSoundType(null))
+        .hardnessAndResistance(main.getBlockHardness(main.getDefaultState(), null, null)));
     this.rawName = reg;
     this.setRegistryName(reg);
-
-//    super(main.getMaterial(main.getDefaultState()), main.getMaterial(main.getDefaultState()).getMaterialMapColor());
-//    this.setSoundType(main.getSoundType());
-//    this.setHardness(main.getBlockHardness(main.getDefaultState(), null, null));
     ingredient = i;
   }
-
   public String rawName(){
     return rawName;
   }
